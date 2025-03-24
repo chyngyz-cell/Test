47dcd6fbf92146053cdff3ab12a6417a0da34e20
chyngyz000 <cyngyzdogdurbaev59@gmail.com>
2025-03-24 15:19:54 +0600

    ASD

--- ddd/src/Main.java
+++ ddd/src/Main.java
@@ -5,4 +5,25 @@ Open clouce Principale: данный прицип гласит
 
 Inter face: данны принсип гласит о там что
 ghwbg bydthcbc принцип инверсис: данны     прицип гласит о том что наши модули верхнего уровня не должны зависит модулей нижнего уровня
- */
+
+
+ git init git@github.com:chyngyz-cell/Test.git
+ git add " название папки" или .
+ git commit -m "Название комита"
+ git branch -M " названте ветки"
+ git remote add origin git@github.com:chyngyz-cell/Test.git
+ git push origin
+
+ git clone  "Сылка" git@github.com:ITPL99/example_git.git
+ git add .
+ git commit "Название"
+ git push origin
+
+ git checkout -b Название ветки от какой ветки наследуемся
+ пример git checkout dev master
+ git commit -am "Название"
+ git checkout Название ветки от которой наследуемся
+ git merge --no-ff Название ветки которую создали
+ git push origin master
+ git push origin Название ветки
+  */
